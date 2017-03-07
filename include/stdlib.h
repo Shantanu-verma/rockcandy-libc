60bc270c187b9b9f338257162bbfd40831ba1e16
Breadpudding <CodeBreadpudding@gmail.com>
2017-03-07 08:49:10 -0500

    Started the creation of stdlib

---
+++ include/stdlib.h
@@ -0,0 +1,10 @@
+typedef unsigned int size_t;
+typedef short wchar_t; // Is this correct?
+// TODO: div_t
+// TODO: ldiv_t
+
+#define NULL 0
+#define EXIT_FAILURE 1
+#define EXIT_SUCCESS 0
+#define RAND_MAX 18446744073709551615
+// TODO: MB_CUR_MAX

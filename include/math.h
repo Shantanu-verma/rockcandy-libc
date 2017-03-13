0c0bdd9785bde12274491a8f3ff908fd3b9af629
Shantanu <shantanuverma@protonmail.com>
2017-03-13 21:08:31 +0530

    so many TODO

--- include/math.h
+++ include/math.h
@@ -1,1 +1,27 @@
+#ifndef _rc_math
+#define _rc_math
 
+double sin (double);
+double cos (double);
+double tan (double);
+double sinh (double);
+double cosh (double);
+double tanh (double);
+double asin (double);
+double acos (double);
+double atan (double);
+double atan2 (double, double);
+double exp (double);
+double log (double);
+double log10 (double);
+double pow (double, double);
+double sqrt (double);
+double ceil (double);
+double floor (double);
+double fabs (double);
+double ldexp (double, int);
+double frexp (double, int*);
+double modf (double, double*);
+double fmod (double, double);
+
+#endif

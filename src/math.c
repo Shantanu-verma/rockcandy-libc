3c3586527b2ce7a90116568dff98603b2da4b645
Shantanu <shantanuverma@protonmail.com>
2017-03-13 21:42:22 +0530

    added functions
    Added abs() , pow() , floor() ,ceil()

--- src/math.c
+++ src/math.c
@@ -1,1 +1,32 @@
+typedef unsigned int uint32_t
 
+// Minimum cycles
+long long abs(long long value){
+  uint32_t _temp = value >> 31;  
+  value ^= _temp;           
+  value += _temp & 1; 
+  return value;
+  }
+  
+// floor
+double floor(double num){
+  double exp = num - (long)num;
+  return ( num - exp );
+}
+
+// ceil
+double ceil(double num){
+  double exp = num - (long)num;
+  return ( num + ( 1 - exp ));
+}
+
+// I think its not very fast
+double pow(double p, unsigned q){
+   double answer = 1;
+   uint32_t it = 0;
+   while(++it <= q)
+     answer*=p;
+   return answer;
+}
+
+  
